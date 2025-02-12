01e955cbe51a229fc58b90fdee60e6e3402f99e5
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2025-02-13 00:28:11 +0100

    Update test_twi.py

--- test_twi.py
+++ test_twi.py
@@ -214,6 +214,130 @@ class TestUntitled:
         self.driver.save_screenshot('screenshot.png')
         specific_text = self.capture_first_five_lines()
 
+        # Upload the screenshot to Twitter
+        upload_url = "https://upload.twitter.com/1.1/media/upload.json"
+        auth = OAuth1(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        
+        with open('screenshot.png', 'rb') as image_file:
+            files = {'media': image_file}
+            response = requests.post(upload_url, files=files, auth=auth)
+        
+        if response.status_code != 200:
+            print("Failed to upload media:", response.status_code, response.text)
+            return
+        
+        media_id = response.json()['media_id_string']
+
+        # Add alt text to the uploaded image
+        alt_text = "This is an automated tweet 🤖\n\nPosition, league, age and metrics were chosen randomly in the 2024/25 dataset.\n\nPlayer age and team refer to their age and team during the season.\n\nPositions are determined via the player's average heat map.\n\nSubscribe to DataMB Pro for more leagues and tools!"  # Add your alt text here
+        metadata_url = "https://upload.twitter.com/1.1/media/metadata/create.json"
+        metadata_payload = {
+    "media_id": media_id,
+    "alt_text": {"text": alt_text}
+}
+        metadata_response = requests.post(metadata_url, json=metadata_payload, auth=auth)
+
+        if metadata_response.status_code != 200:
+            print("Failed to create metadata:", metadata_response.status_code, metadata_response.text)
+            return
+        
+        selected_metric = selected_metric.replace(" per 90", "")
+        selected_position = selected_position.replace("er", "ers")
+        selected_position = selected_position.replace("ack", "acks")
+        selected_position = selected_position.replace("All positions", "Players")
+        selected_age = selected_age.replace("Age", "")
+
+
+        
+
+
+        # Create the tweet text dynamically
+        tweet_text = f"{selected_league} {selected_age} {selected_position} : {selected_metric}\n\n{specific_text}\n\n📊 datamb.football"
+        tweet_text = tweet_text.replace("  ", " ")
+        tweet_text = tweet_text.replace(" Wanderers", "")
+        tweet_text = tweet_text.replace("Borussia ", "")
+        tweet_text = tweet_text.replace("Deportivo ", "")
+        tweet_text = tweet_text.replace("Manchester", "Man")
+        tweet_text = tweet_text.replace(" Hotspur", "")
+        tweet_text = tweet_text.replace("West Ham United", "West Ham")
+        tweet_text = tweet_text.replace("Celta de", "Celta")
+        tweet_text = tweet_text.replace("Olympique Lyonnais", "Lyon")
+        tweet_text = tweet_text.replace("Olympique Marseille", "Marseille")
+        tweet_text = tweet_text.replace("Fortuna ", "")
+        tweet_text = tweet_text.replace("Eintracht ", "")
+        tweet_text = tweet_text.replace("Newcastle United", "Newcastle")
+        tweet_text = tweet_text.replace("🇧🇪 Belgium", "🇧🇪 Belgium Pro League")
+        tweet_text = tweet_text.replace("🏴󠁧󠁢󠁳󠁣󠁴󠁿 Scotland", "🏴󠁧󠁢󠁳󠁣󠁴󠁿 Scottish Premiership")
+        tweet_text = tweet_text.replace("🇦🇹 Austria", "🇦🇹 Austrian Bundesliga")
+        tweet_text = tweet_text.replace("🇨🇭 Switzerland", "🇨🇭 Swiss Super League")
+        tweet_text = tweet_text.replace("🇹🇷 Türkiye", "🇹🇷 Süper Lig")
+        tweet_text = tweet_text.replace("🇩🇰 Denmark", "🇩🇰 Superliga")
+        tweet_text = tweet_text.replace("🇸🇪 Sweden", "🇸🇪 Allsvenskan")
+        tweet_text = tweet_text.replace("🇳🇴 Norway", "🇳🇴 Eliteserien")
+        tweet_text = tweet_text.replace("🇭🇷 Croatia", "🇭🇷 Croatia HNL")
+        tweet_text = tweet_text.replace("🇷🇸 Serbia", "🇷🇸 SuperLiga")
+        tweet_text = tweet_text.replace("🇨🇿 Czech Republic", "🇨🇿 Czech First League")
+        tweet_text = tweet_text.replace("🇵🇱 Poland", "🇵🇱 Ekstraklasa")
+        tweet_text = tweet_text.replace("🇺🇦 Ukraine", "🇺🇦 Premier League")
+        tweet_text = tweet_text.replace("🇷🇺 Russia", "🇷🇺 Premier League")
+        tweet_text = tweet_text.replace("🇬🇷 Greece", "🇬🇷 Super League")
+        tweet_text = tweet_text.replace("🇯🇵 Japan", "🇯🇵 J1 League")
+        tweet_text = tweet_text.replace("🇰🇷 Korea", "🇰🇷 K League 1")
+        tweet_text = tweet_text.replace("🇸🇦 Saudi Arabia", "🇸🇦 Saudi Pro League")
+        tweet_text = tweet_text.replace("🇺🇸 United States", "🇺🇸 MLS")
+        tweet_text = tweet_text.replace("🇲🇽 Mexico", "🇲🇽 Liga MX")
+        tweet_text = tweet_text.replace("🇧🇷 Brazil", "🇧🇷 Série A")
+        tweet_text = tweet_text.replace("🇦🇷 Argentina", "🇦🇷 Primera División")
+        tweet_text = tweet_text.replace("🇺🇾 Uruguay", "🇺🇾 Primera División")
+        tweet_text = tweet_text.replace("🇨🇱 Chile", "🇨🇱 Primera División")
+        tweet_text = tweet_text.replace("🇨🇴 Colombia", "🇨🇴 Primera A")
+        tweet_text = tweet_text.replace("🇪🇨 Ecuador", "🇪🇨 Serie A")
+        tweet_text = tweet_text.replace("🇵🇾 Paraguay", "🇵🇾 Primera División")
+        tweet_text = tweet_text.replace("Short / medium", "Short")
+        tweet_text = tweet_text.replace("short / medium", "short")
+        tweet_text = tweet_text.replace("7 Leagues", "7 League")
+        tweet_text = tweet_text.replace("5 Leagues", "5 League")
+        tweet_text = tweet_text.replace("Wingers", "Wingers & Att Mid")
+        
+
+        time.sleep(150*60)
+
+        # Create the tweet with the media attached
+        tweet_url = "https://api.twitter.com/2/tweets"
+        payload = {
+            "text": tweet_text,
+            "media": {
+                "media_ids": [media_id]
+            }
+        }
+        
+        response = requests.post(tweet_url, json=payload, auth=auth)
+        
+        if response.status_code == 201:
+            print("Tweet successfully sent!")
+            first_tweet_id = response.json()['data']['id']
+            
+            # Create the follow-up tweet
+            follow_up_text = "Compare Top 7 League players, or subscribe to access more leagues, metrics, and tools ⤵️ datamb.football"
+            follow_up_payload = {
+                "text": follow_up_text,
+                "reply": {
+                    "in_reply_to_tweet_id": first_tweet_id
+                }
+            }
+
+            
+            # Send the follow-up tweet
+            follow_up_response = requests.post(tweet_url, json=follow_up_payload, auth=auth)
+            
+            if follow_up_response.status_code == 201:
+                print("Follow-up tweet successfully sent!")
+            else:
+                print("Failed to send follow-up tweet:", follow_up_response.status_code, follow_up_response.text)
+
+        else:
+            print("Failed to send tweet:", response.status_code, response.text)
+
         
 
 
